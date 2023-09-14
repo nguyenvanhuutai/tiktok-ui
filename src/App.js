2e6cfe55afc974cd5fc1106dd021e1923e82de9f
HuuTaiOne <nguyentaint979@gmail.com>
2023-09-14 19:16:05 +0700

    [C1] - Cấu hình Router/Layout cho dự án

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
+import { Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from '~/routes';
-import Home from '~/pages/Home';
-import Following from '~/pages/Following';
+
+import { DefaultLayout } from '~/components/Layout';
 
 function App() {
     return (
         <Router>
             <div className="App">
-                <h1>React App</h1>
                 <Routes>
                     {publicRoutes.map((route, index) => {
                         const Page = route.component
-                        return <Route key={index} path={route.path} element={<Page />} />;
+
+                        let Layout = DefaultLayout
+
+                        if(route.layout){
+                            Layout = route.layout
+                        }else if(route.layout === null){
+                            Layout = Fragment
+                        }
+
+                        return <Route 
+                                    key={index} 
+                                    path={route.path} 
+                                    element={
+                                        <Layout>
+                                            <Page />
+                                        </Layout>
+                                    } />;
                     })}
                 </Routes>
             </div>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,19 @@
+// Layouts
+import { HeaderOnly } from '~/components/Layout'
+
+
 import Following from "~/pages/Following";
 import Home from "~/pages/Home";
 import Profile from "~/pages/Profile";
+import Upload from "~/pages/Upload";
+import Search from "~/pages/Search";
 
 const publicRoutes = [
-    { path: '/', component: Home},
-    { path: '/following', component: Following},
-    { path: '/profile', component: Profile},
+    { path: '/',            component: Home},
+    { path: '/following',   component: Following},
+    { path: '/profile',     component: Profile},
+    { path: '/upload',      component: Upload, layout: HeaderOnly },
+    { path: '/search',      component: Search, layout: null },
 ];
 const priveteRoutes = [];
 
