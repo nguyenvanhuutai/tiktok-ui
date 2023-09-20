76f9fb9f5fcd01625986402457e1c6939c8bcb0d
HuuTaiOne <nguyentaint979@gmail.com>
2023-09-20 12:40:45 +0700

    [C11] - Xây dựng Logic phần Header #2

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -16,8 +16,29 @@ function Search() {
     const [searchValue, setSearchValue] = useState('');
     const [searchResult, setSearchResult] = useState([]);
     const [showResult, setShowResult] = useState(true);
+    const [loading, setLoading] = useState(false)
 
     const inputRef = useRef();
+    
+    
+    useEffect(() => {
+        if(!searchValue.trim()){
+            setSearchResult([]) 
+            return;
+        } 
+
+        setLoading(true)
+        
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+            .then((res) => res.json())
+            .then((res) => {
+                console.log(res);
+                setSearchResult(res.data)
+                setLoading(false)
+            }).catch(() => {
+                setLoading(false)
+            });
+    }, [searchValue]);
 
     const handleClear = () => {
         setSearchValue('');
@@ -29,11 +50,6 @@ function Search() {
         setShowResult(false);
     };
 
-    useEffect(() => {
-        setTimeout(() => {
-            setSearchResult([1, 2, 3]);
-        }, 0);
-    }, []);
     return (
         <HeadlessTippy
             interactive
@@ -42,10 +58,9 @@ function Search() {
                 <div className={cx('search-result')} tabIndex="-1" {...attrs}>
                     <PopperWrapper>
                         <h4 className={cx('search-title')}>Accounts</h4>
-                        <AccountItem />
-                        <AccountItem />
-                        <AccountItem />
-                        <AccountItem />
+                        {searchResult.map((result) => (
+                            <AccountItem key={result.id} data={result} />
+                        ))}
                     </PopperWrapper>
                 </div>
             )}
@@ -60,13 +75,13 @@ function Search() {
                     onChange={(e) => setSearchValue(e.target.value)}
                     onFocus={() => setShowResult(true)}
                 />
-                {!!searchValue && (
+                {!!searchValue && !loading &&  (    
                     <button className={cx('clear')} onClick={handleClear}>
                         <FontAwesomeIcon icon={faCircleXmark} />
                     </button>
                 )}
 
-                {/* <FontAwesomeIcon className={cx('loading')} icon={faSpinner} /> */}
+                {loading && <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />}
 
                 <button className={cx('search-btn')}>
                     <SearchIcon />
